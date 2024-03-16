6d28d204cb61c77da75708b99d713139c3e1543e
Dolity <jakkit852789@gmail.com>
2024-03-17 02:10:13 +0700

    update ...

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const app = express();
 const userRouter = require("./router/route");
 const { initMySQL } = require("./database/database");
 
-const port = 8000
+const port = 24691
 
 app.use(express.json());
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ["https://tame-loincloth-bass.cyclic.app", "https://front-booking-hotels.vercel.app"],
-    // origin: ["http://localhost:8888", "http://localhost:5173"],
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: ["https://tame-loincloth-bass.cyclic.app", "https://front-booking-hotels.vercel.app"],
+//     // origin: ["http://localhost:8888", "http://localhost:5173"],
+//   })
+// );
 
 app.use("/api/v1/user", userRouter)
 
